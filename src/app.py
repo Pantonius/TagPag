a888183fa90ba14eed623128cada4c6b122bf5e7
Julian Schelb <julian.schelb@uni-konstanz.de>
2023-07-18 18:57:06 +0200

    UI improvements and fixed problem with tag selection not updated in UI

--- src/app.py
+++ src/app.py
@@ -7,6 +7,7 @@ import streamlit.components.v1 as components
 import streamlit as st
 from utils.database import *
 from utils.files import *
+from components.welcome import WelcomePage
 
 # ================================= CONFIG ================================
 
@@ -47,58 +48,31 @@ if 'annotator_id' not in st.session_state:
 if 'task_object_id' not in st.session_state:
     STATE.task_object_id = "63e6b1425b2943de553ee687"
 
-# Set webpage ID
+# Sgow first webpage by default
 if 'task_id' not in st.session_state:
     STATE.task_id = 0
 
 # Get tasks
 if 'tasks' not in st.session_state:
-    # if not STATE.task_object_id:
     with st.spinner('Loding tasks...'):
-        tasks = fetchTasks(db, 1, "PROCESSED-STATIC", 100,
-                           {"_id": 1, "landing_url": 1, "content_requests": 1, "status_code": 1, "annotations": 1})
-    STATE.tasks = tasks
-    # else:
-    #    ...
-    # print(len(TASKS), end="\n\n")
+        query = {"batch_id": 1, "status": "PROCESSED-STATIC", "limit": 100}
+        fields = {"_id": 1, "landing_url": 1, "target_url": 1,
+                  "content_requests": 1, "annotations": 1}
+        STATE.tasks = fetchTasks(db, **query, fields=fields)
+
 
 # Shorthand variables
 tasks = STATE.tasks
 task = STATE.tasks[STATE.task_id]
 annotator_id = STATE.annotator_id
 task_url = STATE.tasks[STATE.task_id]['landing_url']
-task_labels_current = task.get('annotations', {}).get(
-    annotator_id, {}).get('labels', [])
+target_url = STATE.tasks[STATE.task_id]['target_url']
 
 # ================================= FUNCTIONS ===============================
 
 
-def set_user_id():
-    """
-    Set the user ID and update the annotator ID in the STATE object.
-
-    This function retrieves the user ID from the STATE object and sets it as the annotator ID.
-    It also updates the query parameters with the annotator ID using st.experimental_set_query_params().
-
-    """
-    user_id = STATE.input_user_id
-    STATE.annotator_id = user_id
-    st.experimental_set_query_params(annotator_id=user_id)
-
-
 def display_webpage(iframe_content, task):
-    """
-    Display the webpage content in an iframe based on the task information.
-
-    This function takes the iframe_content and task information as input.
-    If the task has a valid file_id, it retrieves the page content from the database/file system and displays it in the iframe.
-    Otherwise, it constructs an iframe with the landing_url from the task and displays it.
-
-    Args:
-        iframe_content (object): The object to display the webpage content.
-        task (dict): The task containing the webpage information.
-
-    """
+    """ Display the webpage content in an iframe."""
     url = task.get('landing_url')
     file_id = task.get('content_requests')
 
@@ -112,60 +86,39 @@ def display_webpage(iframe_content, task):
 
 
 def save_annotations(task, annotator_id, annotations):
-    """
-    Save the annotations for a given task and display a confirmation message.
+    """Save the annotations in the database for a given task """
 
-    This function saves the annotations for a specific task in a database or file.
-    It then displays a confirmation message with the tags that were saved.
-
-    Args:
-        task (dict): The task to save the annotations for.
-        annotator_id (str): The ID of the annotator.
-        annotations (dict): The annotations to be saved.
-
-    """
     task_obj_id = task.get('_id')
 
     # TODO: Commented out for testing purposes
     # if task_obj_id:
     #    updateTask(db, task_obj_id, annotator_id, annotations)
 
-    tag_labels = ', '.join(annotations.get("labels", []))
-    st.success(f'Annotation saved! Webpage tagged as "{tag_labels}"')
+
+def update_annotations():
+    """Update the annotations for the current task."""
+
+    try:
+        task = STATE.tasks[STATE.task_id]
+        annotations = task.setdefault("annotations", {})
+        annotations[STATE.annotator_id] = {
+            "labels": STATE.selected_tags
+        }
+        save_annotations(task, STATE.annotator_id, annotations)
+    except (KeyError, TypeError) as e:
+        print("An error occurred while updating the task annotations:", e)
 
 
 def go_to_next_task():
-    """
-    Update the task annotations for the current task and move to the next task.
-
-    This function updates the annotations for the current task based on the selected tags in the STATE object.
-    It catches and handles potential errors such as KeyError and TypeError that may occur during the update.
-    After successfully updating the annotations, it increments the task ID in the STATE object to move to the next task.
-    """
-
-    try:
-        task = STATE.tasks[STATE.task_id]
-        annotations = task.setdefault("annotations", {})
-        annotations[STATE.annotator_id] = {
-            "labels": STATE.selected_tags
-        }
-    except (KeyError, TypeError) as e:
-        print("An error occurred while updating the task annotations:", e)
-    else:
-        STATE.task_id += 1
+    """Advance to the next task."""
+    update_annotations()
+    STATE.task_id += 1
 
 
 def go_to_prev_task():
-    try:
-        task = STATE.tasks[STATE.task_id]
-        annotations = task.setdefault("annotations", {})
-        annotations[STATE.annotator_id] = {
-            "labels": STATE.selected_tags
-        }
-    except (KeyError, TypeError) as e:
-        print("An error occurred while updating the task annotations:", e)
-    else:
-        STATE.task_id -= 1
+    """Go back to the previous task."""
+    update_annotations()
+    STATE.task_id -= 1
 
 # ---------------------------------------------------------------------------
 #                            Layout
@@ -176,42 +129,35 @@ def go_to_prev_task():
 
 # Ask for annotator ID if not provided
 if not STATE.annotator_id:
-    with st.form("form_user_id"):
 
-        # Display warning message if no user ID is provided
-        st.error('No user ID detected. Please enter a user ID below.', icon="🚨")
-        user_id = st.text_input("User ID", key='input_user_id')
-        submit_button = st.form_submit_button("Submit", on_click=set_user_id)
-
-        # Handle form submission
-        if submit_button:
-            if user_id:
-                st.success(f"User ID '{user_id}' submitted successfully!")
-            else:
-                st.warning("Please enter a User ID.")
+    WelcomePage(st).show()
 
 # ================================= MAIN SCREEN ===============================
 
 else:
 
+    # # Tabs
+    # tab_names = ["Snapshot", "Current", "More Info", "All Tasks"]
+    # tab_snapshot, tab_current, tab_info, tab_list = st.tabs(tab_names)
+
+    # # TAB: Display webpage snapshot
+    # with tab_snapshot:
+    #     container = st.container()
+    #     container.warning(
+    #         "Please be aware that webpage snapshots may appear distorted.")
+    #     container.iframe_content = st.empty()
+    #     display_webpage(container.iframe_content, task)
+
+    # # TAB: Display current version of webpage
+    # with tab_current:
+    #     st.warning(
+    #         'If the webpage is not loading properly, please open it in a new tab to ensure proper functionality.')
+    #     st.write(
+    #         f'<iframe src="{task_url}" width="100%" height="1024px" style="border:none;"></iframe>', unsafe_allow_html=True)
+
     # Tabs
-    tab_names = ["Snapshot", "Current", "More Info", "All Tasks"]
-    tab_snapshot, tab_current, tab_info, tab_list = st.tabs(tab_names)
-
-    # TAB: Display webpage snapshot
-    with tab_snapshot:
-        container = st.container()
-        container.warning(
-            "Please be aware that webpage snapshots may appear distorted.")
-        container.iframe_content = st.empty()
-        display_webpage(container.iframe_content, task)
-
-    # TAB: Display current version of webpage
-    with tab_current:
-        st.warning(
-            'If the webpage is not loading properly, please open it in a new tab to ensure proper functionality.')
-        st.write(
-            f'<iframe src="{task_url}" width="100%" height="1024px" style="border:none;"></iframe>', unsafe_allow_html=True)
+    tab_names = ["More Info", "All Tasks"]
+    tab_info, tab_list = st.tabs(tab_names)
 
     # TAB: Display more info about webpage
     with tab_info:
@@ -225,9 +171,14 @@ else:
     # Sidebar
     with st.sidebar:
         st.title('Webpage Annotations')
-        st.sidebar.text_input('Webpage URL:', task_url)
-        st.sidebar.multiselect('Select tags:', LABELS,
-                               default=task_labels_current, key='selected_tags')
+        st.sidebar.text_area('Webpage URL:', task_url)
+        with st.sidebar.expander("More Infos"):
+            st.text_area('Target URL:', target_url)
+
+        STATE.selected_tags = task.get('annotations', {}).get(
+            annotator_id, {}).get('labels', [])
+        st.sidebar.multiselect(
+            'Select tags:', LABELS, key='selected_tags', on_change=update_annotations)
 
         # Navigation buttons
         with st.sidebar.container():
@@ -235,6 +186,7 @@ else:
             col1.button('Previous', use_container_width=True,
                         on_click=go_to_prev_task)
             col2.button('Next', use_container_width=True,
+                        disabled=STATE.selected_tags == [],
                         on_click=go_to_next_task)
 
             st.sidebar.warning(
