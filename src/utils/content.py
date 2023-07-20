fcb8493b35cb936d3b87caadce6b950226311570
Julian Schelb <julian.schelb@uni-konstanz.de>
2023-07-20 14:42:12 +0200

    Display extracted text

---
+++ src/utils/content.py
@@ -0,0 +1,22 @@
+from bs4 import BeautifulSoup
+
+
+def extractText(html_document: str):
+    """Extracts the actual text from an HTML document"""
+
+    soup = BeautifulSoup(html_document, features="html.parser")
+
+    # Kill all script and style elements
+    for script in soup(["script", "style"]):
+        script.extract()  # Rip it out
+
+    # Get text
+    text = soup.get_text()
+    # Break into lines and remove leading and trailing space on each
+    # lines = (line.strip() for line in text.splitlines())
+    # Break multi-headlines into a line each
+    # chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+    # Drop blank lines
+    # text = "\n".join(chunk for chunk in chunks if chunk)
+
+    return text
