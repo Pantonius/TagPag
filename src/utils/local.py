14418787b13629db3124e23c3a6a2e064423a900
Roberto Ulloa <roberto.ur@gmail.com>
2024-06-27 16:29:14 +0200

    remove files.py

--- src/utils/local.py
+++ src/utils/local.py
@@ -1,4 +1,5 @@
 import os
+import json
 import pandas as pd
 
 TASKS_FILE = 'data/tasks.csv' # TODO: Move this to .env (mayhaps)
@@ -102,4 +103,30 @@ def getPageContent(id: str):
             return f.read()
     except FileNotFoundError:
         return None
-    
+
+
+def read_json_file(file_path: str) -> dict:
+    """
+    Read and parse a JSON file.
+
+    Args:
+        file_path (str): The path to the JSON file.
+
+    Returns:
+        dict: The parsed JSON data as a dictionary.
+
+    Raises:
+        FileNotFoundError: If the file specified by `file_path` is not found.
+        json.JSONDecodeError: If there is an error decoding the JSON data.
+        Exception: If any other error occurs while reading the JSON file.
+    """
+    try:
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+            return data
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Error: File '{file_path}' not found.")
+    except json.JSONDecodeError:
+        raise json.JSONDecodeError(f"Error: Failed to decode JSON in file '{file_path}'.")
+    except Exception as e:
+        raise Exception(f"Error: An error occurred while reading the JSON file: {str(e)}")
